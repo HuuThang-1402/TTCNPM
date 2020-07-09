8b4f78b501b6b8f449c6318bb94c826f2d8f5137
huuthang <thang.nguyen2018@hcmut.edu.vn>
2020-07-09 22:18:37 +0700

    Add QuenMatKhauForn.cs file

---
+++ QuenMatKhauForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BKFoodCourt
+{
+    public partial class QuenMatKhauForm : Form
+    {
+        public QuenMatKhauForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new EmailForm().Show();
+        }
+    }
+}
