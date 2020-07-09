75ebc2ad3356e8eb6ce75bbb56151639d79801c5
huuthang <thang.nguyen2018@hcmut.edu.vn>
2020-07-09 22:33:22 +0700

    Add doimatkhau.cs file

---
+++ doimatkhau.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BKFoodCourt
+{
+    public partial class doimatkhau : Form
+    {
+        public doimatkhau()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "123")
+            {
+                new MatKhauSai().Show();
+            }
+            else if (textBox2.Text != textBox3.Text)
+            {
+                new MatKhauMoi().Show();
+            }
+            else
+            {
+                this.Hide();
+                new ThanhCong().Show();
+            }
+        }
+    }
+}
