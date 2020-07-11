8b3373410f4eeaa5a760ae2e1c98225b9e9e6e47
huuthang <thang.nguyen2018@hcmut.edu.vn>
2020-07-11 23:18:31 +0700

    Add BinhDan.cs file

---
+++ BinhDan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BKFoodCourt
+{
+    public partial class BinhDan : UserControl
+    {
+        private static BinhDan _instance;
+        public static BinhDan Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new BinhDan();
+                return _instance;
+            }
+        }
+        public BinhDan()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
