7495d179a0e9ee1e1b2cc61552febffd8a77fd78
HuuThang-1402 <pmtpmtpmtpmt20@gmail.com>
2020-07-12 09:33:18 +0700

    Add HangTonKho.cs file

---
+++ HangTonKho.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BKFoodCourt
+{
+    public partial class HangTonKho : UserControl
+    {
+        private static HangTonKho _instance;
+        public static HangTonKho Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new HangTonKho();
+                return _instance;
+            }
+        }
+        public HangTonKho()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void HangTonKho_Load(object sender, EventArgs e)
+        {
+
+            string absolutePath = System.IO.Path.GetFullPath("Hangtonkho.txt");
+            Helper.file = absolutePath;
+            dataGridView1.DataSource = Helper.DataTableFromTextFile("Hangtonkho.txt");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
