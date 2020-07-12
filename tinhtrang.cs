c9b236a3cde3f635f05118976c4a1195e12ad49c
HuuThang-1402 <nguyenhuuthang2510828@gmail.com>
2020-07-12 10:11:51 +0700

    Add tinhtrang.cs file

---
+++ tinhtrang.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKFoodCourt
+{
+    class tinhtrang
+    {
+        
+            public string MonAn { get; set; }
+            public decimal SoLuong { get; set; }
+            public string TinhTrang { get; set; }
+            public string UocTinh { get; set; }
+        
+    }
+}
