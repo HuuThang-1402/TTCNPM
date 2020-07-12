8ce4d3cdf8c2edc0db13fe5e39716d3f86c77916
HuuThang-1402 <nguyenhuuthang2510828@gmail.com>
2020-07-12 10:10:40 +0700

    Add ThongTinNhanVien.cs

---
+++ ThongTinNhanVien.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BKFoodCourt
+{
+    public partial class ThongTinNhanVien : Form
+    {
+        public ThongTinNhanVien()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void ThongTinNhanVien_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = NhanVien.ten;
+            textBox2.Text = NhanVien.ngay;
+            textBox3.Text = NhanVien.tuoi;
+            textBox4.Text = NhanVien.sdt;
+            textBox5.Text = NhanVien.diachi;
+            textBox6.Text = NhanVien.calam;
+            textBox7.Text = NhanVien.luong;
+            textBox8.Text = NhanVien.note;
+        }
+    }
+}
