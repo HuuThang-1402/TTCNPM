f0c0b0ce836b1ea02f3c38df3940c1aeb8eaf153
HuuThang-1402 <nguyenhuuthang2510828@gmail.com>
2020-07-12 10:01:08 +0700

    Add order.cs file

---
+++ order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKFoodCourt
+{
+    class order
+    {
+        public string MonAn { get; set; }
+        public decimal SoLuong { get; set; }
+        public decimal Gia { get; set; }
+    }
+}
