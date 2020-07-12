06c033b15b3f33ca78d310ab938de7485f93c1a8
HuuThang-1402 <nguyenhuuthang2510828@gmail.com>
2020-07-12 10:08:58 +0700

    Add Thongtincanhan.cs file

---
+++ Thongtincanhan.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BKFoodCourt
+{
+    public partial class Thongtincanhan : Form
+    {
+        public Thongtincanhan()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.ReadOnly = false;
+            textBox2.ReadOnly = false;
+            textBox3.ReadOnly = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text;
+            textBox2.Text = textBox2.Text;
+            textBox3.Text = textBox3.Text;
+            textBox1.ReadOnly = true;
+            textBox2.ReadOnly = true;
+            textBox3.ReadOnly = true;
+        }
+    }
+}
